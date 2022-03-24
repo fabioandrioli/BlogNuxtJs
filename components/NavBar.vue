b2648aa3a4877c27ee2ece14be3c8651600d6f9a
Fabio Gilberto <fabio.goncalves@bomjesus.br>
2022-03-24 16:20:44 -0300

    First commit

---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="py-1 bg-">
+    <ul>
+      <li>Home</li>
+      <li>Categoria</li>
+      <li>Noticia</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style>
+
+</style>
